3d76c60bb6a898e8e6dbc2dba38a742ebf55802b
DevStuki <devstuki@gmail.com>
2018-10-22 20:42:20 +0900

    2018/10/22 - 2

--- String/7kyu-Mumbling.js
+++ String/7kyu-Mumbling.js
@@ -12,7 +12,25 @@ The parameter of accum is a string which includes only letters from a..z and A..
  */
 
 function accum(s) {
+    var arr = s.split("").map((c, i) => times(c, i))
+                         .map(c => c.join(""))
+                         .map(str => str.replace(/.+/, x=> x.toLowerCase()))
+                         .map(str => str.replace(/^./, x => x.toUpperCase()));
+    return arr.join("-");
+}
 
+function times(char, num){
+    let str = [];
+    for(let i = 0; i < num+1; i++){
+        str.push(char);
+    }
+    return str;
+}
+
+//Best Practice
+//repeat()을 처음본다. 확인해둘것!!
+function accum(s){
+    return s.split("").map((c, i) => (c.toUpperCase() + c.toLowerCase().repeat(i))).join("-");
 }
 
 console.log(accum("ZpglnRxqenU"));
